ff7c46cf963862fb683a579fd7899d87178335be
Marko Zabreznik <marko@zabreznik.net>
2022-04-20 11:42:02 +0200

    task: upgrade packages and build scripts

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-yarn ncc build src/deploy.ts -o bin
-
-echo '#!/usr/bin/env node\n' > ./bin/spa-deploy-cli
-cat ./bin/index.js >> ./bin/spa-deploy-cli
-chmod +x ./bin/spa-deploy-cli
-rm ./bin/index.js
