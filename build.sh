6a1e367a45a2ccf1cc9f87bc77517d7da1c7db5c
Tadej Stanic <tadej.stanic@povio.com>
2022-05-11 09:56:22 +0200

    fix build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 yarn ncc build src/deploy.ts -o bin
 
-echo '#!/usr/bin/env node\n' > ./bin/spa-deploy-cli
+echo '#!/usr/bin/env node' > ./bin/spa-deploy-cli
 cat ./bin/index.js >> ./bin/spa-deploy-cli
 chmod +x ./bin/spa-deploy-cli
 rm ./bin/index.js
